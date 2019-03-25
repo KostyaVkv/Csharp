1526c9129b38184df6d7c8728ec6f4edd374cab3
KostyaVkv <48446830+KostyaVkv@users.noreply.github.com>
2019-03-25 19:42:35 +0300

    Занятие 25.03.19

--- GameProject/GameProject/MainWindow.xaml.cs
+++ GameProject/GameProject/MainWindow.xaml.cs
@@ -13,7 +13,6 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using GameMaps;
-
 namespace GameProject
 {
     /// <summary>
@@ -23,26 +22,23 @@ namespace GameProject
     {
         UniversalMap_Wpf map;
         TimerController timer = new TimerController();
-        int FireX = 0;
-        int FireY = 0;
-        int FireSaveX = 0;
-        int FireSaveY = 0;
+        Players Player = new Players();
         int[] SetGemX = new int[3];
         int[] SetGemY = new int[3];
         int WallX = 200;
         int WallY = 300;
-        int PlayerWalk=3;
+        int PlayerWalk = 3;
         int PlayerRun;
+        int FirstGemPosition = 0;
+        int WallCounter = 0;
         Random r = new Random();
         Gem[] Gems = new Gem[3];
-
-
-
         public MainWindow()
         {
             InitializeComponent();
             map = MapCreator.CreateMap(this, 38, 20, 50);
             Gem.map = map;
+            Players.map = map;
             map.Library.ImagesFolder = new PathInfo { Path = "..\\..\\images", Type = PathType.Relative };
             map.Library.AddPicture("wall", "wall.png");
             map.Library.AddPicture("fire", "fire.png");
@@ -52,15 +48,15 @@ namespace GameProject
             map.Library.AddPicture("stones", "stones.jpg");
             map.SetMapBackground("stones");
             PlayerRun = PlayerWalk * 2;
-            map.Library.AddContainer("fire", "fire");
-            map.ContainerSetSize("fire", 50, 50);
-            map.ContainerSetCoordinate("fire", FireX, FireY);
-            map.ContainerSetZIndex("fire", 102);
-            map.ContainerSetIndents("fire", 5, 5);
             map.Library.AddContainer("wall", "wall");
             map.ContainerSetSize("wall", 50, 50);
             map.ContainerSetCoordinate("wall", WallX, WallY);
             CreateGems();
+            CreatePlayer();
+            map.Library.AddContainer("fon", "wall", ContainerType.TiledImage);
+            map.ContainerSetSize("fon", 50, 500);
+            map.ContainerSetTileSize("fon", 50, 50);
+            map.ContainerSetCoordinate("fon",25, 25);
             
             timer.AddAction(CheckKey, 10);
             //Done:Доделать движение: устранить эффект перепрыгивания стены, вернув координаты в исходное состояние; добавить 2 кнопки.
@@ -68,7 +64,6 @@ namespace GameProject
             //Done:Сделать так, чтобы после сбора кристалла он появлялся в случайном месте, но не ближе 100 пикселей к игроку.
             //Создать класс для кристала.
             //* Продумать механику игры.
-
         }
         /// <summary>
         /// Функция создания контейнера 
@@ -78,13 +73,29 @@ namespace GameProject
         /// <param name="y"></param>
         /// <param name="z">Индекс</param>
         /// <param name="Picturename">Название Картинки из биоблитеки</param>
-        void CreateContainers(string ContainerName,  int z, string Picturename)
+        void CreateContainers(string ContainerName, int z, string Picturename)
         {
             map.Library.AddContainer(ContainerName, Picturename);
             map.ContainerSetSize(ContainerName, 50, 50);
             map.ContainerSetIndents(ContainerName, 5, 5);
-         
             map.ContainerSetZIndex(ContainerName, z);
+        }
+        void CreateWalls(int LeftTopX,int LeftTopY,int RightBottomX, int RightBottomY)
+        {
+            map.Library.AddContainer("Wall" + WallCounter.ToString() ,"wall", ContainerType.TiledImage);
+            map.ContainerSetSize("Wall" + WallCounter.ToString(), 50, 500);
+            map.ContainerSetTileSize("Wall" + WallCounter.ToString(), 50, 50);
+            //TODO:Доделать позиционирование контейнера со стеной (размер, координаты).
+            //TODO:Задание с массивами 2,3 из предыдущего урока
+            //TODO:Сделать массив стен проверку движения через стену.
+        }
+
+        void CreatePlayer()
+        {
+            Player.ContainerName = "Fire";
+            Player.Picture = "fire";
+            CreateContainers(Player.ContainerName, 102, Player.Picture);
+            map.ContainerSetCoordinate(Player.ContainerName, Player.X, Player.Y);
         }
         void CreateGems()
         {
@@ -93,80 +104,123 @@ namespace GameProject
                 Gems[i] = new Gem();
                 Gems[i].ContainerName = "Gem" + i.ToString();
                 Gems[i].Picture = "Gem" + i.ToString();
-               
                 CreateContainers(Gems[i].ContainerName, 101, Gems[i].Picture);
                 SetGemRandomCoordinate(Gems[i]);
-
             }
         }
         void CollectGems()
         {
             for (int i = 0; i < Gems.Length; i++)
             {
-                if (map.CollisionContainers("fire", "Gem" + i.ToString()))
+                if (map.CollisionContainers("Fire", "Gem" + i.ToString()))
                 {
                     SetGemRandomCoordinate(Gems[i]);
                 }
             }
         }
-        
         void SetGemRandomCoordinate(Gem G)
         {
+            int x = r.Next(0, 1820);
+            int y = r.Next(0, 980);
+            int i = r.Next(1, 5);
+            if (FirstGemPosition == 0)
+            {
+                int FirstX = 500 + r.Next(1, 5) * 100;
+                int FirstY = 500 - r.Next(1, 5) * 100;
+                FirstX = FirstX - r.Next(1, 5) * 10;
+                FirstY = FirstY - r.Next(1, 5) * 10;
+                x = FirstX;
+                y = FirstY;
+                FirstGemPosition++;
+            }
+            else
+            {
+                if (Player.X - x <= 100 || Player.Y - y <= 100 || x - Player.X <= 100 || y - Player.Y <= 100)
+                {
+                    if (Player.X < 100 || Player.Y < 100)
+                    {
+                        x += 101;
+                        y += 101;
+                    }
+                    if (Player.X > 1720 || Player.Y > 880)
+                    {
+                        x -= 101;
+                        y -= 101;
+                    }
+                    if (Player.X > 1720 || Player.Y < 100)
+                    {
+                        x -= 101;
+                        y += 101;
+                    }
+                    if (Player.X < 100 || Player.Y > 880)
+                    {
+                        x += 101;
+                        y -= 100;
+                    }
+                }
+                if (x >= 1820 || y >= 980 || x < 0 || y < 0)
+                {
+                    x = 500 - i * 100;
+                    y = 500 + i * 100;
+                    x = 500 + i * 10;
+                    y = 500 - i * 10;
+                }
+            }
             //TODO:Сделать нормальнеы рандомные координаты
-            G.SetCoordinates(r.Next(0, 500), r.Next(0, 500));
+            G.SetCoordinates(x, y);
+        }
+        void PlayerMove(int nx,int ny)
+        {
+            map.ContainerMovePreview(Player.ContainerName, nx, ny, 0);
+            if (!(map.CollisionContainers(Player.ContainerName, "wall", true)))
+            {
+                Player.SetCoordinates(nx, ny);
+                CollectGems();
+            }
+            //else
+            //{
+            //    x = Player.SaveX;
+            //    y = Player.SaveY;
+            //    map.ContainerSetCoordinate("Fire", x, y);
+            //}
+            //Player.PlayerMoving(x, y, xs, ys);
         }
         void CheckKey()
         {
-            FireSaveX = FireX;
-            FireSaveY = FireY;
             bool Moving = false;
             int Speed;
+            int x = Player.X;
+            int y = Player.Y;
             if (map.Keyboard.IsKeyPressed(Key.LeftShift))
                 Speed = PlayerRun;
             else
+            {
                 Speed = PlayerWalk;
+            }
             if (map.Keyboard.IsKeyPressed(Key.W))
             {
-                FireY -= Speed;
-
+                y -= Speed;
                 Moving = true;
             }
             if (map.Keyboard.IsKeyPressed(Key.A))
             {
-                FireX -= Speed;
-
+                x -= Speed;
                 Moving = true;
             }
             if (map.Keyboard.IsKeyPressed(Key.D))
             {
-                FireX += Speed;
-
+                x += Speed;
                 Moving = true;
             }
             if (map.Keyboard.IsKeyPressed(Key.S))
             {
-                FireY += Speed;
-
+                y += Speed;
                 Moving = true;
             }
-
             if (Moving)
             {
-                map.ContainerMovePreview("fire", FireX, FireY, 0);
-                if (!(map.CollisionContainers("fire", "wall", true)))
-                {
-                    map.ContainerSetCoordinate("fire", FireX, FireY);
-                    CollectGems();
-                }
-
-                else
-                {
-                    FireX = FireSaveX;
-                    FireY = FireSaveY;
-                    map.ContainerSetCoordinate("fire", FireX, FireY);
-                }
+                PlayerMove(x,y);
             }
-
         }
     }
     class Gem
@@ -174,63 +228,27 @@ namespace GameProject
         public int X { get; private set; }
         public int Y { get; private set; }
         static public UniversalMap_Wpf map;
-        public string Picture ;
+        public string Picture;
         public string ContainerName;
-        public void SetCoordinates(int x,int y)
+        public void SetCoordinates(int x, int y)
         {
             X = x;
             Y = y;
-            map.ContainerSetCoordinate(ContainerName,x,y);
+            map.ContainerSetCoordinate(ContainerName, x, y);
         }
     }
-
-    
-    class Coordinates
+    class Players
     {
-        public int X = 0;
-        public int Y = 0;
-        Random r = new Random();
-        Coordinate[] SetGem = new Coordinate[3];
-        public void GetGem(int FireX, int FireY, int i)
+        public int X { get; private set; }
+        public int Y { get; private set; }
+        static public UniversalMap_Wpf map;
+        public string Picture;
+        public string ContainerName;
+        public void SetCoordinates(int x, int y)
         {
-            SetGem[i] = new Coordinate();
-
-            SetGem[i].X = r.Next(0, 1820);
-            SetGem[i].Y = r.Next(0, 980);
-            if (FireX - SetGem[i].X <= 100 || FireY - SetGem[i].Y <= 100 || SetGem[i].X - FireX <= 100 || SetGem[i].Y - FireY <= 100)
-            {
-                if (FireX < 100 || FireY < 100)
-                {
-                    SetGem[i].X += 101;
-                    SetGem[i].Y += 101;
-                }
-                if (FireX > 1720 || FireY > 880)
-                {
-                    SetGem[i].X -= 101;
-                    SetGem[i].Y -= 101;
-                }
-                if (FireX > 1720 || FireY < 100)
-                {
-                    SetGem[i].X -= 101;
-                    SetGem[i].Y += 101;
-                }
-                if (FireX < 100 || FireY > 880)
-                {
-                    SetGem[i].X += 101;
-                    SetGem[i].Y -= 100;
-                }
-            }
-            if (SetGem[i].X >= 1820 || SetGem[i].Y >= 980 || SetGem[i].X < 0 || SetGem[i].Y < 0)
-            {
-                SetGem[i].X = 500 - i * 100;
-                SetGem[i].Y = 500 + i * 100;
-                SetGem[i].X = 500 + i * 10;
-                SetGem[i].Y = 500 - i * 10;
-
-            }
-
+            X = x;
+            Y = y;
+            map.ContainerSetCoordinate(ContainerName, x, y);
         }
-
-
     }
 }
---
+++ Домашка/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    { 
+        
+        static void Main(string[] args)
+        {
+            int[,] homework = new int[8, 8];
+            for(int i=0;i<8;i++)
+            {
+                for(int j=0;j<8;j++)
+                {
+                    homework[i, j] = 1 + j + i;
+
+                }
+            }
+            NonMain(homework);
+            Console.ReadKey();
+        }
+        static void NonMain(int [,] a)
+        {
+            for(int i = 0;i<a.GetLength(0);i++)
+            {
+                for(int j=0;j<a.GetLength(1);j++)
+                {
+                    
+                    Console.Write(a[i, j]);
+                 
+                    Console.Write("\t");
+                }
+                Console.WriteLine();
+            }
+            
+        }
+    }
+}
